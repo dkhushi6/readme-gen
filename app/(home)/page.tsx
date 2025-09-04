7a60b577690c6886b95e782f49309877ed09aef9
Khushi Dhankhar <khushidhankhar006@gmail.com>
2025-09-04 23:44:33 +0530

    feat: integrate GitHub API to fetch user public repositories

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,18 +1,48 @@
 "use client";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import axios from "axios";
 import { useSession } from "next-auth/react";
-import React from "react";
+import { useRouter } from "next/navigation"; // ✅ use next/navigation instead of next/router in app dir
+import React, { useState } from "react";
 
-const page = () => {
+const Page = () => {
   const { data: session } = useSession();
+  const router = useRouter();
+  const [username, setUsername] = useState("");
 
-  if (session) {
-    console.log(session);
-  }
+  // Handle typing
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setUsername(e.target.value.trim());
+  };
+
+  // Handle button click
+  const handleSearch = async () => {
+    if (!username) return;
+    try {
+      const res = await axios.post("/api/public/all-repo", { username });
+      if (res.data) {
+        // router.push(`/repos/${username}`);
+        console.log("data", res.data);
+      }
+    } catch (err) {
+      console.error("Error fetching repos:", err);
+    }
+  };
+
   return (
     <div className="text-5xl flex justify-center items-center h-screen">
-      HOME PAge
+      <div className="flex gap-2">
+        <Input
+          type="text"
+          value={username}
+          onChange={handleInputChange}
+          placeholder="Enter GitHub username"
+        />
+        <Button onClick={handleSearch}>Get Repos</Button>
+      </div>
     </div>
   );
 };
 
-export default page;
+export default Page;
---
+++ app/api/public/all-repo/route.ts
@@ -0,0 +1,16 @@
+import axios from "axios";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(req: NextRequest) {
+  const body = await req.json();
+  const { username } = body;
+  if (!username) {
+    return NextResponse.json({ message: "username not found" });
+  }
+  const repos = await axios.get(
+    `https://api.github.com/users/${username}/repos`
+  );
+  console.log(repos);
+  //fetch data from the response
+  return NextResponse.json({ success: true, repos: repos.data });
+}
